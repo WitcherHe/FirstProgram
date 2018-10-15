a56e78cd54989059f19b12e4fbc27d8e03f886dd
WitcherHE <guyuezhiye@163.com>
2018-10-15 17:02:20 +0800

    Create component header, footer, sidebar, content, r-sidebar

---
+++ src/app/r-sidebar/r-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-r-sidebar',
+  templateUrl: './r-sidebar.component.html',
+  styleUrls: ['./r-sidebar.component.css']
+})
+export class RSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
