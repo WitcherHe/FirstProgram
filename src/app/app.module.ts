997ceb9bc73ebbd2d7697e21c4f85bc1269a2c47
WitcherHe <31182650+WitcherHe@users.noreply.github.com>
2018-10-18 01:47:23 +0800

    Error check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { RSidebarComponent } from './r-sidebar/r-sidebar.component';
-import { GundamManagementComponent } from './gundam-management/gundam-management.component';
+import { GundamManagementComponent } from './gundam/gundam-management/gundam-management.component';
 import { StarComponent } from './star/star.component';
 
 
--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-star',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarComponent implements OnInit {
 
-  @Input()
-  public rate: number = 0;
+  @Input() 
+  rate: number = 0;
 
-  private stars: Array<boolean>;
+  private stars: boolean[];
 
   constructor() { 
     
